de9ad673220061fd73a4b000e56b06bc0ad4cc0f
Felipe Kenji Takata <lipe.takata@gmail.com>
2026-09-17 09:57:30 -0300

    feat: codificando os pedidos e carrinho

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -25,15 +25,66 @@ const Pedido = () => {
     }
 
     const produtosDisponiveis = items.filter(item => item.disponivel);
-    const carrinho = items.filter(item.quantidade > 0);
+    const carrinho = items.filter(item=>item.quantidade > 0);
 
-    const subTotal = carrinho.reduce((act, item)=> act.item.preco * item.quantidade, 0);
+    const subTotal = carrinho.reduce((act, item)=> act + item.preco * item.quantidade, 0);
     const total = subTotal > 0 ? subTotal + taxaEntrega: 0;
 
+    const confirmarPedido=() => {
+        setEnviar(true);
+        setStatus("Restaurante Preparando o Pedido");
+        setTimeout(()=>{
+            setStatus("Seu Pedido saiu para entrega!");
+            setEnviar(false);
+        },5000)
+        setTimeout(()=>{
+            setStatus("Seu pedido foi entregue com sucesso!");
+            setEnviar(false);
+        },10000)
+    }
+
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>Cardápio do Restaurante</h1>
+      {produtosDisponiveis.map(produto=>(
+        <div key={produto.id}> 
+            <span>{produto.nome}(R${produto.preco.toFixed(2)})</span>
+            <div>
+                <button onClick={()=>AlterarQuantidade(produto.id, -1)}>-</button>
+                <span>{produto.quantidade}</span>
+                <button onClick={()=>AlterarQuantidade(produto.id, +1)}>+</button>
+            </div>
+        </div>
+      ))}
+
+      <hr></hr>
+      <h3>Resumo da Entrega</h3>
+      {carrinho.length === 0 ?(
+        <p>Seu Carrinho está vazio</p>
+      ):(
+        <>
+        <ul>
+            {carrinho.map(item =>(
+                <li key={item.id}>
+                    {item.quantidade} X {item.nome} - R$ {total.toFixed(2)}
+
+                </li>
+            ))}
+        </ul>
+        <p>SubTotal - R${subTotal.toFixed(2)}</p>
+        <p>Taxa de Entrega: R${taxaEntrega.toFixed(2)}</p>
+        <button onClick={confirmarPedido} disabled={enviar}>
+            {enviar ? "enviando ..." : "Confirmar Pedido"}
+        </button>
+        </>
+      )}
+      {status && (
+        <div>
+            <strong>Alerta:</strong>{status}
+        </div>
+      )}
+
+    </div>
   )
 }
 
